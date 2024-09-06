faaf8101b6ad1a92aa74cc3b17702981ebf1ff7d
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-09-06 20:48:30 +0200

    alert added

--- createBlog.mjs
+++ createBlog.mjs
@@ -1,26 +1,3 @@
-// import { getInputValues } from "/formUtils.mjs";
-// import { createBlogPost } from "/apiUtils.mjs";
-
-// document.getElementById("blogForm").addEventListener("submit", async function (e) {
-//   e.preventDefault();
-
-//   const user = JSON.parse(localStorage.getItem("CreateBlog-user"));
-//   if (!user) {
-//     alert("User not logged in.");
-//     return;
-//   }
-
-//   const payload = getInputValues();
-
-//   try {
-//     await createBlogPost(user, payload);
-//     alert("Blog post created successfully!");
-//     location.reload();
-//   } catch (error) {
-//     alert("Error creating blog post: " + error.message);
-//   }
-// });
-
 const user = JSON.parse(localStorage.getItem("CreateBlog-user"));
 const { accessToken, name: userName } = user;
 
@@ -58,6 +35,7 @@ document.getElementById("blogForm").addEventListener("submit", async function (e
 
     if (response.ok) {
       console.log("Blog successfully processed");
+      alert("Success! Your blog has been created.");
       blogForm.reset();
     } else {
       console.error("Error processing blog:", await response.json());
